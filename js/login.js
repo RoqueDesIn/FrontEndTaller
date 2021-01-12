6b0267f0abf186f146938de0f6879502b8b1ba09
Roque1211 <roque1211@gmail.com>
2021-01-12 08:55:35 +0100

    First commit

---
+++ js/login.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $("#go").click(function(){
+        var username = $("#login").val().trim();
+        var password = $("#password").val().trim();
+
+        if( username != "" && password != "" ){
+            $.ajax({
+                url: 'https://localhost:4436/login',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({username:username,password:password}),
+                success: function(data, status){
+                    //alert("Data: " + data + "\nStatus: " + status);
+                    if(data){ 
+                        window.location.replace("../views/welcome.html");
+                    } else {
+                        $("#error").css("display","block");
+                    }
+              }
+            })
+        }
+    });
+});
