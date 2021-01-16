915ddb161c1c85a67e5b9a38d5aa07846bc89acd
alvar <alvar@DESKTOP-K8EHG61>
2021-01-16 14:15:36 +0100

    Comportamientoos del backend completados e implentados en resumen

--- js/resumen.js
+++ js/resumen.js
@@ -20,4 +20,25 @@ $(document).ready(function () {
 
 		}
 	});
+	$.ajax({
+		url: 'https://localhost:44346/Resum/UserResum',
+		dataType: 'json',
+		type: 'get',
+		contentType: 'application/json',
+		success: function (data, status) {
+			//Por cada elemento dentro del array data, construye una fila (tr)
+			//y añade celdas con los campos de cada elemento del array.
+			$.each(data, function (i, item) {
+				var $tr = $('<tr>').append(
+					$('<td>').text(item.nombre),
+					$('<td>').text(item.cantidad),
+					$('<td>').text(item.valor),
+				); //.appendTo('#records_table');
+				console.log($tr.wrap('<p>').html());
+				$('#User').append($tr);
+			});
+
+		}
+	});
+
 });
